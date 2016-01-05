da1673996aa8869a48051948164fcf0895f99231
Yogender Kumar <dagar.yogenderkumar@gmail.com>
2016-01-06 01:57:05 +0530

    setting angular application

---
+++ angular/app-services/authentication.service.js
@@ -0,0 +1,49 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('AuthenticationService', AuthenticationService);
+
+    AuthenticationService.$inject = ['$http', '$cookieStore', '$rootScope', '$timeout', 'UserService'];
+    function AuthenticationService($http, $cookieStore, $rootScope, $timeout, UserService) {
+        var service = {};
+
+        service.Login = Login;
+        service.SetCredentials = SetCredentials;
+        service.ClearCredentials = ClearCredentials;
+
+        return service;
+
+        function Login(userData, callback) {
+
+
+            $http.post('/api/authenticate', userData)
+                .success(function (response) {
+                    callback(response);
+                });
+
+        }
+
+        function SetCredentials(userData) {
+            var authdata = Base64.encode(userData.username + ':' + userData.password + ':' + userData.rememberMe);
+
+            $rootScope.globals = {
+                currentUser: {
+                    username: userData.username,
+                    authdata: authdata
+                }
+            };
+
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata; // jshint ignore:line
+            $cookieStore.put('globals', $rootScope.globals);
+        }
+
+        function ClearCredentials() {
+            $rootScope.globals = {};
+            $cookieStore.remove('globals');
+            $http.defaults.headers.common.Authorization = 'Basic';
+        }
+    }
+
+})();
